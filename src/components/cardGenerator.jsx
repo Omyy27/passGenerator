838a08e8c1a46fb340c6417a2b7e95882c478fa7
Omyy27 <omovisolutions@gmail.com>
2022-09-04 12:06:07 -0500

    final version

--- src/components/cardGenerator.jsx
+++ src/components/cardGenerator.jsx
@@ -1,31 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import generator from "generate-password";
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+
 const CardGenerator = () => {
   const [range, setRange] = useState(4);
-  const [checkeds, setCheckeds] = useState([1,2,3,4]);
-  const [strg, setStrg] = useState([])
-
+  const [checkeds, setCheckeds] = useState([1, 2, 3, 4]);
+  const [strg, setStrg] = useState([]);
+  const [show, setShow] = useState(false);
+  const [strength, setStrength] = useState('');
 
   const cases = [
     {
       id: 1,
       name: "uppercase letters",
-      func: getRandomUpper,
     },
     {
       id: 2,
       name: "lowercase letters",
-      func: getRandomLower,
     },
     {
       id: 3,
       name: "numbers",
-      func: getRandomNumber,
     },
     {
       id: 4,
       name: "symbols",
-      func: getRandomSymbol,
     },
   ];
 
@@ -41,63 +42,67 @@ const CardGenerator = () => {
     }
   }
 
-  const getPass = (arr, length) => {
-    let content = [];
-    const Lis = arr.map(el => {
-      // eslint-disable-next-line
-       const arFn = cases.filter(function (f) {
-         if (f.id === el) {
-           return f.func;
-         }
-       });
-       return arFn;
-     });
-    //  console.log(Lis);
-    for (let i = 0; i < (length/2); i++) {
-      // const arFn = arr.map(t => )
-      const item = getterFun(Lis);
-      content.push(item)
+  useEffect(() => {
+    const sumall = checkeds.map(item => item).reduce((prev, curr) => prev + curr, 0);
+    if (sumall <= 5) {
+      setStrength('Basic 🥵')
+    } else if(sumall > 5 && sumall <= 8){
+      setStrength('Medium ☹️')
+    }else{
+      setStrength('High 😎')
     }
-    setStrg(content)
-    return content;
-  }
+  }, [checkeds])
+  
 
-  function getterFun(arrFuncy) {
-    let forStr = [];
-    arrFuncy.forEach(el => {
-      const psItem = el[0].func();
-      forStr.push(psItem);
+  const getPass = () => {
+    const pwd = generator.generate({
+      length: range,
+      lowercase: checkeds.includes(2),
+      uppercase: checkeds.includes(1),
+      numbers: checkeds.includes(3),
+      symbols: checkeds.includes(4),
     });
-    console.log(forStr);
-    return forStr.join('');
-  }
-  function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-  }
+    setStrg(pwd);
+  };
 
-  function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-  }
+  // async function copyPageUrl(e) {
+  //   try {
+  //     await navigator.clipboard.writeText(e.target.value);
+  //     console.log('Page URL copied to clipboard');
+  //     setShow(true);
 
-  function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-  }
+  //   } catch (err) {
+  //     console.error('Failed to copy: ', err);
+  //   }
+  // }
 
-  function getRandomSymbol() {
-    const symbols = "!@#$%*&(){}[]=<>/,.";
-    return symbols[Math.floor(Math.random() * symbols.length)];
+  function copyPageUrl(e) {
+    e.target.select();
+    document.execCommand("copy");
+    setShow(true);
   }
 
   return (
     <>
-    <div className="mb-3">
+      <Alert show={show} variant="success">
+        <Alert.Heading>The password was copy to clipboard!</Alert.Heading>
+        <div className="d-flex justify-content-end">
+          <Button onClick={() => setShow(false)} variant="outline-success">
+            X
+          </Button>
+        </div>
+      </Alert>
+      <div className="mb-3">
         <input
           type="text"
           className="form-control text-white"
           id="inputpass"
           readOnly
           placeholder="P4$5W0rD!"
-          value={strg.join('')}
+          value={strg}
+          onClick={(e) => {
+            copyPageUrl(e);
+          }}
         />
       </div>
       <div className="card">
@@ -136,10 +141,14 @@ const CardGenerator = () => {
           <div className="col-12 px-3 d-flex flex-row flex-wrap justify-content-between align-items-center content-strength my-3">
             <span className="text-muted fw-bold">STRENGTH</span>
             <div>
-              <h5 className="text-white fw-bold m-0">BASIC 🥵</h5>
+              <h5 className="text-white fw-bold m-0 text-uppercase">{strength}</h5>
             </div>
           </div>
-          <button type="button" onClick={()=>getPass(checkeds, range)} className="btn btn-success btn-block rounded-0">
+          <button
+            type="button"
+            onClick={() => getPass(checkeds, range)}
+            className="btn btn-success btn-block rounded-0"
+          >
             GENERATE →
           </button>
         </div>
